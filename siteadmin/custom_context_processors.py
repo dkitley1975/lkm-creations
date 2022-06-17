7c89ff666ceba33c412b4257de35e12e46a450f1
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-06-17 23:42:01 +0100

    refactor: the site info context processor

--- siteadmin/custom_context_processors.py
+++ siteadmin/custom_context_processors.py
@@ -3,14 +3,15 @@ from siteadmin.models import SiteInfo
 
 def site_info(request):
     """
-    Return the site information for the site.
-    """
+    Return the site information object from the database.
+    filters the by is_active = True, orders by created_at in descending order.
+    and then trims the results to the first one."""
+
     site_info = (
         SiteInfo.objects.all()
         .filter(is_active=True)
-        .order_by("-created_at")[0:1]
+        .order_by("-created_at")[0]
     )
-    context = {
+    return {
         "site_info": site_info,
     }
-    return context
