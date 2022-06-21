c2832b26680b521976bbd5dd30a5b2149ecb1a88
David Kitley-McNamara <david@kitley-mcnamara.com>
2022-06-21 14:02:32 +0100

    fix: added the default receipient email

--- core/settings.py
+++ core/settings.py
@@ -156,6 +156,7 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_RECIPIENT_ADDRESS = os.environ.get("EMAIL_HOST_USER")
 
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_REQUIRED = True
